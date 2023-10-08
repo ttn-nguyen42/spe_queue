aebde5a4632d847cf08aab32687301f064017d72
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2023-10-08 16:11:18 +0700

    MMN0208: Individual server monitor in progress

--- servers.py
+++ servers.py
@@ -1,4 +1,5 @@
 from visitor import Visitor
+from functools import wraps
 import simpy as sp
 import params as pr
 import numpy as np
@@ -17,7 +18,46 @@ class VisitorServer:
 
     def stop(self):
         pass
+    
+    def patch_resource(resource, pre=None, post=None):
+        """Patch *resource* so that it calls the callable *pre* before each
+        put/get/request/release operation and the callable *post* after each
+        operation.  The only argument to these functions is the resource
+        instance.
+        
+        """
+        def get_wrapper(func):
+            # Generate a wrapper for put/get/request/release
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                # This is the actual wrapper
+                # Call "pre" callback
+                if pre:
+                    pre(resource)
 
+                # Perform actual operation
+                ret = func(*args, **kwargs)
+
+                # Call "post" callback
+                if post:
+                    post(resource)
+
+                return ret
+            return wrapper
+
+        # Replace the original operations with our wrapper
+        for name in ['put', 'get', 'request', 'release']:
+            if hasattr(resource, name):
+                setattr(resource, name, get_wrapper(getattr(resource, name)))
+
+    def monitor(data, resource):
+        """This is our monitoring callback."""
+        item = (
+            resource._env.now,  # The current simulation time
+            resource.count,  # The number of users
+            len(resource.queue),  # The number of queued processes
+        )
+        data.append(item)
 
 class ReceptionServer(VisitorServer):
     def __init__(self, env: sp.Environment, params: pr.ServerParams) -> None:
